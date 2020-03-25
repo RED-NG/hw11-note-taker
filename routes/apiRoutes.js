c4039bfe542991b15698f65967e0bb70605551be
Philip Ng <phil__n@hotmail.com>
2020-03-25 03:40:21 -0400

    post request working properly and added functionality to get/post/delete ajax requests

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,4 +8,18 @@ module.exports = function(app) {
   app.get("/api/notes", (req, res) => {
     res.json(notesDB);
   });
+
+  app.post("/api/notes", (req, res) => {
+    notesDB.push(req.body);
+    console.log(notesDB);
+    for (let i = 0; i < notesDB.length; i++) {
+      notesDB[i].id = i + 1;
+    }
+
+    fs.writeFile("./db/db.json", JSON.stringify(notesDB), err => {
+      if (err) throw err;
+      console.log("Your note has been saved!");
+    });
+    res.json(true);
+  });
 };
