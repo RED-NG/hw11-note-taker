bd0a8fa0bfe9e6f06baf8d691975283d67912b01
Philip Ng <phil__n@hotmail.com>
2020-03-25 18:29:53 -0400

    delete api route working properly

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,7 +9,7 @@ module.exports = function(app) {
     res.json(notesDB);
   });
 
-  app.post("/api/notes", (req, res) => {
+  app.post("/api/noted", (req, res) => {
     notesDB.push(req.body);
     console.log(notesDB);
     for (let i = 0; i < notesDB.length; i++) {
@@ -20,6 +20,22 @@ module.exports = function(app) {
       if (err) throw err;
       console.log("Your note has been saved!");
     });
+    res.json(true);
+  });
+
+  app.delete("/api/delete/:id", (req, res) => {
+    for (let i = 0; i < notesDB.length; i++) {
+      if (req.params.id == notesDB[i].title) {
+        notesDB.splice(i);
+      }
+    }
+    fs.writeFile(
+      path.join(__dirname, "../db/db.json"),
+      JSON.stringify(notesDB),
+      err => {
+        if (err) throw err;
+      }
+    );
     res.json(true);
   });
 };
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 // Dependencies
 // =============================================================
-var express = require("express");
+const express = require("express");
+const bodyParser = require("body-parser");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
-var bodyParser = require("body-parser");
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
