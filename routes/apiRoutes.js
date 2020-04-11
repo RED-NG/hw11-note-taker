9d5ba023ea6fe9e33b8ca95edee19c7682dabc17
Philip Ng <phil__n@hotmail.com>
2020-04-11 18:05:31 -0400

    api route for deleting notes works

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,39 +3,42 @@
 const notesDB = require("../db/db.json");
 const fs = require("fs");
 const path = require("path");
+const id = require("shortid");
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.get("/api/notes", (req, res) => {
-    res.json(notesDB);
+    fs.readFile("./db/db.json", "utf8", function (err, db) {
+      if (err) {
+        console.log("get request for notes has failed" + err);
+        return;
+      }
+      var db = JSON.parse(db);
+      res.json(db);
+    });
   });
 
-  app.post("/api/noted", (req, res) => {
-    notesDB.push(req.body);
-    console.log(notesDB);
+  app.post("/api/notes", (req, res) => {
+    var newNote = req.body;
     for (let i = 0; i < notesDB.length; i++) {
-      notesDB[i].id = i + 1;
+      newNote.id = i + 1;
     }
+    console.log(newNote);
+    notesDB.push(newNote);
+    console.log(notesDB);
 
-    fs.writeFile("./db/db.json", JSON.stringify(notesDB), err => {
+    fs.writeFile("./db/db.json", JSON.stringify(notesDB), (err) => {
       if (err) throw err;
-      console.log("Your note has been saved!");
     });
+    console.log("Your note has been saved!");
     res.json(true);
   });
 
   app.delete("/api/delete/:id", (req, res) => {
     for (let i = 0; i < notesDB.length; i++) {
-      if (req.params.id == notesDB[i].title) {
-        notesDB.splice(i);
+      if (req.params.id == notesDB[i].id) {
+        notesDB.splice(i, 1);
       }
     }
-    fs.writeFile(
-      path.join(__dirname, "../db/db.json"),
-      JSON.stringify(notesDB),
-      err => {
-        if (err) throw err;
-      }
-    );
-    res.json(true);
+    res.json(notesDB);
   });
 };
